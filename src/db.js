81068c2202e8e106d42cb9b32c8bb26aa7d37f07
matinic <117241248+matinic@users.noreply.github.com>
2023-09-09 20:22:55 -0300

    db url

--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const { Sequelize, Op } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_URL } = process.env;
 
 const sequelize = new Sequelize(
-   `postgres://matias:tsjDoCFBsXFQ7ojiqpWvRlusYDPVr9jh@dpg-cjt5hk5htt0c738vhnjg-a/pokemondb_hyez`,
+   DB_URL,
    {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
