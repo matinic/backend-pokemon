8934cee93db5c49f401f0ccfeaae4ab7bfedcc4c
matinic <117241248+matinic@users.noreply.github.com>
2023-09-08 22:32:35 -0300

    all pokemons route

--- src/handlers/pokemon/getAll.js
+++ src/handlers/pokemon/getAll.js
@@ -3,9 +3,10 @@ const getAllPokemonsDb = require("../../controllers_utils/DbControllers/pokemonD
 
 module.exports = async (req, res) => {
   try {
-    const allPokemons = await getAllPokemons();
+    // const allPokemons = await getAllPokemons();
     // const allPokemonsDb = await getAllPokemonsDb();
-    return res.status(200).json([...allPokemons]);
+    // return res.status(200).json([...allPokemons]);
+    res.send('<h2>ALL POKEMONS ROUTE</h2>')
   } catch (error) {
     return res.status(404).json(error.message);
   }
