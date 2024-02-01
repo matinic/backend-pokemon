2bd6e02f1d1fbcdbd3d0687bac6e8a7a9aa7a5ef
matinic <117241248+matinic@users.noreply.github.com>
2024-02-01 17:23:18 -0300

    added port 0 for railway use

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const server = require('./src/app.js');
 const port = process.env.PORT || 3001
 
-server.listen(port, () => {console.log('%s listening at 3001')});
+server.listen(port, "0.0.0.0" ,() => {console.log('%s listening at 3001')});
 
